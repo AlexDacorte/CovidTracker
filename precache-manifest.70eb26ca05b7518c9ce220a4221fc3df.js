c106c688428acb9f784d48774e9dd2ba78a238a1
Alex Da Corte <dacortejhexson@gmail.com>
2020-07-26 09:11:56 -0500

    Updates

---
+++ precache-manifest.70eb26ca05b7518c9ce220a4221fc3df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f9a89d2b2b0f8cbfa976ef77227fa0",
+    "url": "/CovidTracker/index.html"
+  },
+  {
+    "revision": "fc9c1e1200ea2b006edd",
+    "url": "/CovidTracker/static/css/main.a5ba6f6e.chunk.css"
+  },
+  {
+    "revision": "c1d23dde31698d2da6dc",
+    "url": "/CovidTracker/static/js/2.cd1e1996.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/CovidTracker/static/js/2.cd1e1996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc9c1e1200ea2b006edd",
+    "url": "/CovidTracker/static/js/main.748f3093.chunk.js"
+  },
+  {
+    "revision": "03739fdda49687f98d5a",
+    "url": "/CovidTracker/static/js/runtime-main.867e5707.js"
+  },
+  {
+    "revision": "7377bf2c834c4b8e97eb9244bf79fac5",
+    "url": "/CovidTracker/static/media/image.7377bf2c.png"
+  }
+]);
