a49442c9944527b50ef30e52e84a2a88ddd1d4d0
AlexDacorte <dacortejhexson@gmail.com>
2026-05-12 06:35:19 +0200

    updated favicon

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter  as Router, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import CountryDetail from "./pages/CountryDetail";
 import Compare from "./pages/Compare";
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <Router basename="/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/country/:name" element={<CountryDetail />} />
@@ -24,7 +24,7 @@ const App = () => (
           <Route path="/countries" element={<Countries />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </TooltipProvider>
   </QueryClientProvider>
 );
